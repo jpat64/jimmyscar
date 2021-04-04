050e4c581a45facadf63ae13b5a77e26263deb84
jpat64 <jrp3tu@virginia.edu>
2021-04-04 11:51:03 -0500

    added toggle on latest page, kinda

--- jimmyscar/app/controllers/latest_controller.rb
+++ jimmyscar/app/controllers/latest_controller.rb
@@ -16,6 +16,21 @@ class SongDummy
     @owner
   end
 end
+
+class PersonDummy
+  def initialize(name, points)
+    @name = name
+    @points = points
+  end
+
+  def name
+    @name
+  end
+
+  def points
+    @points
+  end
+end
 ## COULD MAYBE BE PORTED OVER TO UNIT TESTS FOR MOCKING IF I HAD ACTUAL DEVELOPMENT STANDARDS ##
 
 
@@ -27,6 +42,8 @@ class LatestController < ApplicationController
     @btier = [SongDummy.new("I'm Feeling You", "Santana & Michelle Williams", "Dustin")]
     @ctier = [SongDummy.new("Left the Car Running", "Whales-Talk", "Joey"), SongDummy.new("Red Rubber Ball", "The Cyrkle", "Sarah W")]
     @dtier = [SongDummy.new("Homecoming", "Kanye West", "Kunal")]
-    @ftier = [SongDummy.new("Kindest Regards", "Some Rapper", "Nobody")]
+    @ftier = [SongDummy.new("Kindest Regards", "Witt Lowry", "Nobody")]
+
+    @people = [PersonDummy.new("Dustin", 11), PersonDummy.new("Sarah B", 12), PersonDummy.new("Katarina", 26), PersonDummy.new("Taylor", 14)]
   end
 end
--- jimmyscar/app/helpers/application_helper.rb
+++ jimmyscar/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def toggle_songs_points
+        if $listing_songs
+            $listing_songs = false
+        else
+            $listing_songs = true
+        end
+    end
 end
